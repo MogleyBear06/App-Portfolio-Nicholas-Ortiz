a8fa01bbbaaa04368ac5405c20b49d078bf6a39d
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2026-04-08 23:52:11 -0500

    bg image on registry

--- client/src/components/pages/Registry.js
+++ client/src/components/pages/Registry.js
@@ -48,8 +48,8 @@ export default function Registry() {
                 position: 'fixed',
                 top: 0,
                 left: 0,
-                width: '100%',
-                height: '100%',
+                 width: "100vw",
+                height: "100vh",
                 objectFit: 'cover',
                 zIndex: -1,
               }}
